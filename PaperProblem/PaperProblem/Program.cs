756cd617a529c7f3aaf61256fb0afcac21f55b87
BlackSun93 <54064095+BlackSun93@users.noreply.github.com>
2019-09-26 09:34:46 +0100

    Add files via upload
    Added a program where a painter has to figure how much paint he needs to paint a room.
    Added a program to solve how many reams of paper and toner cartridges someone needs to print reports for a meeting and how much this will cost.

---
+++ PaperProblem/PaperProblem/Program.cs
@@ -0,0 +1,74 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountMade;
+            //int attendants = 25;
+            int lengthOfReport = 140;
+            int paper = 500;
+            double tonerCartridgesNeeded;//= amountMade / 1200;
+            double costReam, costCart;
+            double paperTotal;
+            double cartTotal;
+
+
+            amountMade = (25 + 5) * lengthOfReport; //25 attendants + 5 extra, finds how many pages are needed
+            double totalReamsNeeded = System.Math.Ceiling( amountMade / Convert.ToDouble(paper));
+           // totalReamsNeeded = System.Math.Ceiling(totalReamsNeeded); //rounds reams up as they have to be bought in stacks of 500
+
+            tonerCartridgesNeeded = amountMade / 1200; //1 cartridge lasts for 1200 pages
+            tonerCartridgesNeeded = System.Math.Ceiling(tonerCartridgesNeeded); // rounds up
+
+            Console.WriteLine("Please enter the cost of each ream of paper:");
+            costReam = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the cost per toner cartridge:");
+            costCart = Convert.ToDouble(Console.ReadLine());
+
+            if (totalReamsNeeded >= 5)
+            {
+                paperTotal = (costReam / 1.1) * totalReamsNeeded;
+                Console.WriteLine("As over 5 reams of paper have been ordered, a discount of {0} has been applied. ", costReam / 1.1);
+            }
+            else
+            {
+                paperTotal = costReam * totalReamsNeeded;
+            }
+
+            if (tonerCartridgesNeeded >= 5)
+            {
+                cartTotal = (costCart / 1.1) * tonerCartridgesNeeded;
+                Console.WriteLine("AS over 5 cartridges are needed, a discount of {0} has been applied", costCart / 1.1);
+            }
+            else
+            {
+                cartTotal = costCart * tonerCartridgesNeeded;
+            }
+            double finalCost = paperTotal + cartTotal;
+            Console.WriteLine("You need {0} reams of paper and {1} amount of cartridges. The paper will cost £ {2} and the cartridges £ {3}. the total cost is {4}", totalReamsNeeded, tonerCartridgesNeeded, Math.Round(paperTotal, 2), Math.Round(cartTotal, 2), Math.Round(finalCost, 2));
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+/* 
+    Create a program to solve this problem.
+    Test this problem with the following data: The report is 140 pages long, there will be 25 people at the meeting.
+    Assuming that the printer is old and inefficient, modify the program to calculate how many toner cartridges are also required if each one
+    lasts 1200 pages.
+    Modify the program to calculate the total purchase cost, taking the cost per ream and per cartridge as user input.
+    The local print store offers a 10% discount on bulk purchases of 5 or more reams of paper. It offers the same discount on toner cartridges, 
+    but the two offers cannot be combined (i.e. you do not get a discount on 3 reams and 2 cartridges). 
+    Modify the program to automatically account for this discount - it should also inform the user when a discount has been included.
+*/
---
+++ paintproblem/paintproblem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paintproblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomW = 0, roomL = 0, roomH = 0;
+            double winL, winH;
+            int winAmount;
+            double totalWin = 0;
+            double totalRoom, totalToPaint;
+
+            Console.WriteLine("Hello! Please enter the width, length and height of the room you're painting:");
+            Console.WriteLine("Enter the width in feet:");
+            roomW = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the length in feet:");
+            roomL = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the height in feet:");
+            roomH = Convert.ToDouble(Console.ReadLine());
+
+            totalRoom = (2*((roomH * roomW) + (roomH * roomL) + (roomL * roomW))) - (roomL * roomW); //not painting the floor so take that face away
+            Console.WriteLine("the room's surface area to paint is {0}", totalRoom);
+
+            Console.WriteLine("How many windows are there?");
+                winAmount = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 1; i < winAmount + 1; i++) //starts at 1 so 1st window isnt shown as window 0, winAmount increased accordingly
+            {
+                Console.WriteLine("Please enter the height of window {0} in feet:", i);
+                winH = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the length of window {0} in feet :", i);
+                winL = Convert.ToDouble(Console.ReadLine());
+
+                totalWin = totalWin + (winL * winH); //only asks for height and length as window is a cutout on one face (i.e its 2d)
+            }
+
+            Console.WriteLine("the total surface area of the windows is {0}", totalWin);
+            totalToPaint = totalRoom - totalWin;
+
+            Console.WriteLine("the total area to paint is {0}", totalToPaint);
+
+            double HowManyGallons = totalToPaint / 100; //1 gallon covers 100ft^2
+            Console.WriteLine("You need {0} gallons to paint the room, minus the windows and minus the floor. Thanks", HowManyGallons);
+
+            Console.ReadLine();
+        }
+    }
+}
